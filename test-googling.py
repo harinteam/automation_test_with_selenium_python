93c2543d9027ef579800b06f9e176ed68255525b
adityadenny <aditaydenny6@gmail.com>
2022-03-09 00:48:29 +0700

    use selenium, webdriver, pytest

--- test-googling.py
+++ test-googling.py
@@ -1,13 +1,23 @@
 import time
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-def test_googling():
+
+@pytest.fixture
+def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get('https://www.google.com')
-    driver.find_element_by_name('q').send_keys('Home Official Chelsea FC' + Keys.ENTER)
-    assert 'Home | Official Site | Chelsea Football Club' in driver.find_element_by_css_selector('h3').text
-    assert 'Home | Official Site | Chelsea Football Club' in driver.title
+    yield driver
     driver.quit()
+    time.sleep(60)
+
+
+def test_googling(driver):
+    driver.find_element_by_name('q').send_keys('halrez web dev' + Keys.ENTER)
+    assert 'Halrez Web Dev IT' in driver.find_element_by_css_selector('h3').text
+    assert 'Halrez Web Dev IT' in driver.title
+    # driver.quit()
+    # time.sleep(100)
